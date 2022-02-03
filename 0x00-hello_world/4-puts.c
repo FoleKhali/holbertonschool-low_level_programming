c73a5bc73b3a4398af85f57eac98e8c109670c36
= <=>
2022-02-03 13:38:09 -0800

    create 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main(puts)
+/**
+ * main - Entry point
+ *
+ *return: always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
