6a766475450603ae747c9a12f9a0398872bd82bd
= <=>
2022-02-04 15:18:44 -0800

    create 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - the entire alphabet.
+ * Return: always 0.
+ */
+int main(void)
+{
+
+	char a;
+
+	for(a = 'a'; a <= 'z', a++)
+		putchar(a);
+
+	putchar('\n')
+
+		return (0);
+}
