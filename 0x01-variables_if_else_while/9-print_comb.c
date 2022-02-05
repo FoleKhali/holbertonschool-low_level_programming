4284a148d40af15dbd72d7a406cbcb3fd65c0ff3
= <=>
2022-02-04 16:38:58 -0800

    create 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,14 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x < 10; x++);
+	for (x = 0; x <= 9; x++);
 	{
 		putchar((x % 10) + '0');
 		if (x == 9)
-			continue;
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
 	putchar('\n');
 
