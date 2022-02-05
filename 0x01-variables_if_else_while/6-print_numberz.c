17721e285243f0b4096121369f9e7c6f8040e962
= <=>
2022-02-04 16:01:44 -0800

    create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - numbers again, but with putchar
+ * Return: Always 0.
+ */
+int main(void)
+{
+
+	int n;
+
+	for (n = 0, n < 10; n++)
+		putchar((n % 10), n);
+
+	putchar('\n');
+
+	return (0);
+}
