864e888ccdb3a9a70f62a40254d576e36dab2a6f
= <=>
2022-02-04 16:24:43 -0800

    create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - numbers of base 16 in lowercase.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int h;
+
+	for (h = 0; h < 10; h++)
+		putchar((h % 10) + '0');
+	for (h = 'a'; h <= 'f'; h++)
+		putchar(h);
+	
+	putchar('\n');
+
+	return (0);
+}
