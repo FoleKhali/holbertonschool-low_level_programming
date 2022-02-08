b1306d3363b0de7a510567240a21550c66d89c5c
= <=>
2022-02-08 11:33:58 -0800

    create 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes a character
+ * _putchar - writes a character c.
+ * &c: the character to print
  * Return: On success 1.
  * On error, -1 is returned and error is set appropriately.
  */
