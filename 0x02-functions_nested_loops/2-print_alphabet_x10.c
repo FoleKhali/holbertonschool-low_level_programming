143a94a867ec14bbb2bcfa1010ff1cb1fc19d0a1
= <=>
2022-02-08 14:12:20 -0800

    create 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet two times
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	char o;
+
+	while(o<10)
+	{
+	char n;
+
+	for(n = 'a'; n <= 'z'; n++)
+		_putchar(n);
+	}
+	_putchar(10);
+	o++;
+}
