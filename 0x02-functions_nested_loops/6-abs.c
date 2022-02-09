f04e76a52af6c552b9459fc53a4d6e90314c4505
= <=>
2022-02-08 17:28:31 -0800

    create 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * abs - compute the absolute value of an integer
+ * @o: value
+ * Always: Always 0.
+ */
+int _abs(int o)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+}
