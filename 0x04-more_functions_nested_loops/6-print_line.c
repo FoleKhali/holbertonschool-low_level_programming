659a511ec80717a9ed224511295e850a40bf3254
= <=>
2022-02-10 19:47:49 -0800

    create 6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - Entry point
+ *@n: number of times to print '_'
+ * Return: no return
+ */
+void print_line(int n)
+{
+	int x;
+	char z;
+
+	z = '_';
+	for (x = 0; x < x; n++)
+	{
+		if (n > 0)
+			_putchar(z);
+	}
+	_putchar('\n');
+}
