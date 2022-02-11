cb554aced11f68d0f7869da75e52e619def529e4
= <=>
2022-02-10 16:20:29 -0800

    create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: digit to multiply
+ * @b: digit to multiply
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int c = 0;
+
+	c = a * b;
+	_putchar(c);
+
+	return (c);
+
+}
