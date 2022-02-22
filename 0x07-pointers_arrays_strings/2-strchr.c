4306155271b65b57b6ec6df2911e13176effc5ef
= <=>
2022-02-21 19:52:08 -0800

    create 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,23 +1,29 @@
+#include "main.h"
 
-        unsigned int i;
+/**
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
+char *_strchr(char *s, char c)
+{
+	int a = 0, b;
 
-        for (; ; i++)
-        {
-                if (s[i] == c)
-                        return (&(s[i]));
-                if (!s[i])
-                        return (NULL);
+	while (s[a])
+	{
+		a++;
+	}
 
-        }
-}
-ed int i;
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
 
-        for (; ; i++)
-        {
-                if (s[i] == c)
-                        return (&(s[i]));
-                if (!s[i])
-                        return (NULL);
-
-        }
+	return ('\0');
 }
