ca114fa6dc9760a9d74299a9ddbaa9f87377c29a
= <=>
2022-03-02 04:32:06 -0800

    create 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,36 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * create_array - function that creates an array of chars
- * and initializes it with a specific char
- *
- * @c: array of chars
- * @size: the size of the memory to print
- *
- * Return: address of the memory to print
- */
-
+  * create_array - creates an array of chars
+  * @size: The size of the array
+  * @c: The char to fill in the array
+  *
+  * Return: The array filled
+  */
 char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
-	char *pArray;
+	char *s;
+
+	s = malloc(size * sizeof(char));
 
 	if (size == 0)
 		return (NULL);
 
-/* returns a pointer to the allocated memory */
-	pArray = malloc(size * sizeof(char));
-
-	if (pArray == NULL)
+	if (s == NULL)
 		return (NULL);
 
-	i = 0;
-	while (i < size)
-	{
-		pArray[i] = c;
-		i++;
-	}
-	return (pArray);
+	for (i = 0; i < size; i++)
+		s[i] = c;
+
+	return (s);
 }
