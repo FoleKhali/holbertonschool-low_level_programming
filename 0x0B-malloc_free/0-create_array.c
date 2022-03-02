39f092cbd4f2c107d69d2d2915ee8182753ccb53
= <=>
2022-03-01 19:13:38 -0800

    create 0-create_array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - writes the character c to stdout
+ * @size: The character to print
+ * @c: The character to print
+ * Return: pointer to array or null
+ *
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	a = malloc(sizeof(char) * size);
+
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			a[i] = c;
+		}
+	}
+	return (a);
+
+}
