6be0f4afda2b798db4ab471eb140f10bcf8bc8b8
= <=>
2022-03-09 18:58:20 -0800

    create 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,17 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "function_pointers.h"
-
 /**
- * print_name - Prints a name
- *
- * @name: Name to print.
- *
- * @f: Pointer to a function
- *
- * Return: void
+ * print_name - Function the name
+ * @name: Parameter
+ * @f: Pointer to function
+ * Return : Return void
  */
-
 void print_name(char *name, void (*f)(char *))
 {
-	if (f)
-		f(name);
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+	(f)(name);
 }
