69918a987582af076a2d7e26b8c215068bb0095d
= <=>
2022-03-09 18:46:27 -0800

    create 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,13 @@
 #include "function_pointers.h"
 /**
- * print_name - prints a name depends of the function.
- * @name: points to Name's string.
- * @f: points to function.
- *
- * Return: the string initialized.
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
+
 void print_name(char *name, void (*f)(char *))
 {
 	if (!f || !name)
 		return;
-		(*f)(name);
+	(*f)(name);
 }
