8666523a09ab6c23d71ef5023ff003fa2694cbfe
FoleKhali <dhannaps03@gmail.com>
2022-03-17 21:51:03 -0700

    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

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,5 +10,41 @@
  */
 list_t *add_node(list_t **head, const char *str);
 {
+	list_t *a;
 
+	if (head != NULL && str != NULL)
+	{
+		a = malloc(sizeof(list_t));
+		if (a == NULL)
+			return (NULL);
+
+		a->str = strdup(str);
+		a->len = _strlen(str);
+		a->next = *head;
+
+		*head = a;
+
+		return (a);
+	}
+
+	return (0);
 }
+
+/**
+ * _strlen - Entry point
+ *@s: pointer the string we want to now length
+ *
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+        int i;
+
+        i = 0;
+        while (*(s + i) != '\0')
+        {
+                i++;
+        }
+        return (i);
+}
+
