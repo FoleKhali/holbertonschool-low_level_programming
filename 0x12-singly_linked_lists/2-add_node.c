826287b9e654660637f67a4726ff1b2a29c93909
FoleKhali <dhannaps03@gmail.com>
2022-03-17 20:32:20 -0700

    nothing good

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * add_node - add a new node at the beginning of list_t
+ * @head: linked list
+ * @str: added string
+ * Return: The adress of the new list and NULL if fail
+ */
+list_t *add_node(list_t **head, const char *str);
+{
+
+}
