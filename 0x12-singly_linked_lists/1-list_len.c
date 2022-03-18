8c0ce5351314c1f6cb0e47e937d22502b6c6fc97
FoleKhali <dhannaps03@gmail.com>
2022-03-17 19:38:34 -0700

    successfull compilation

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * list_len - show the number of elements of a list
+ * @h: Linked list
+ * Return: The number required
+ */
+size_t list_len(const list_t *h)
+{
+	size_t a = 0;
+
+	while(h != NULL)
+	{
+	h = h->next;
+	/**
+	 * next makes sure that h points the next pointer in
+	 * the linked list so we prevent to make an infinite loop
+	 */
+	a++;
+	}
+	return(a);
+}
+
