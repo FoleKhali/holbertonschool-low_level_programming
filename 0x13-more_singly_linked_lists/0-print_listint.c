32458cdc4818ab5113bd65f915e6d3906d21071e
FoleKhali <dhannaps03@gmail.com>
2022-03-22 13:27:22 -0700

    1

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,5 +7,17 @@
  */
 size_t print_listint(const listint_t *h)
 {
+	int count = 0;
 
+	if (h != NULL)
+	{
+		while (h)
+		{
+			printf("%d\n", h->n);
+			h = h->next;
+			count++;
+		}
+	}
+
+	return (count);
 }
