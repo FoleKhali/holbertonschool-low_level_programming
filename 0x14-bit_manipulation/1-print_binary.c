2fa59f7975febed8a74fda9703d10574c0604230
FoleKhali <dhannaps03@gmail.com>
2022-03-24 18:44:58 -0700

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,4 @@
-#include "holberton.h"
-#include <stdio.h>
+#include "main.h"
 
 /**
  * print_binary - This function prints the binary
@@ -9,10 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-
-	if (n > 1)
-	{
+	if (n >> 1)
 		print_binary(n >> 1);
-	}
 	_putchar((n & 1) + '0');
 }
