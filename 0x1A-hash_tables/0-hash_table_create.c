11a5c1fb8af05c402dc7d558db933ffa10a13ade
FoleKhali <platanobrgas@gmail.com>
2022-05-27 17:11:05 -0500

    betty fixes

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,10 +1,9 @@
 #include "hash_tables.h"
 
-/*
+/**
  * hash_table_create - create a hash table
  * @size: size of the table
  * Return: New hash table if succses, else NULL
- *
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
